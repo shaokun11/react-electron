961a2bc413379079cbe58249386e78057fd83f84
shaokun <40231565+shaokun11@users.noreply.github.com>
2020-12-03 11:05:31 +0800

    Update electron.js
    移除开发的顶部设置

--- public/electron.js
+++ public/electron.js
@@ -26,64 +26,35 @@ ipcMain.on("userInfo", async () => {
 const setupMenu = () => {
   const menu = new Menu();
   mainWindow.setMenu(menu);
-
   const template = [
-    {
-      label: "Application",
-      submenu: [
-        {
-          label: "About Application",
-          selector: "orderFrontStandardAboutPanel:",
-        },
-        {
-          type: "separator",
-        },
-        {
-          label: "Quit",
-          accelerator: "Command+Q",
-          click: () => {
-            quit();
-          },
-        },
-      ],
-    },
     {
       label: "Edit",
       submenu: [
-        {
-          label: "Undo",
-          accelerator: "CmdOrCtrl+Z",
-          selector: "undo:",
-        },
-        {
-          label: "Redo",
-          accelerator: "Shift+CmdOrCtrl+Z",
-          selector: "redo:",
-        },
-        {
-          type: "separator",
-        },
-        {
-          label: "Cut",
-          accelerator: "CmdOrCtrl+X",
-          selector: "cut:",
-        },
-        {
-          label: "Copy",
-          accelerator: "CmdOrCtrl+C",
-          selector: "copy:",
-        },
-        {
-          label: "Paste",
-          accelerator: "CmdOrCtrl+V",
-          selector: "paste:",
-        },
-        {
-          label: "Select All",
-          accelerator: "CmdOrCtrl+A",
-          selector: "selectAll:",
-        },
+        { role: "undo" },
+        { role: "redo" },
+        { type: "separator" },
+        { role: "cut" },
+        { role: "copy" },
+        { role: "paste" },
+        { role: "delete" },
+        { type: "separator" },
+        { role: "selectAll" },
       ],
+    },
+    {
+      label: "View",
+      submenu: [
+        { role: "reload" },
+        { role: "forcereload" },
+        { type: "separator" },
+        { role: "resetzoom" },
+        { role: "zoomin" },
+        { role: "zoomout" },
+      ],
+    },
+    {
+      label: "Window",
+      submenu: [{ role: "minimize" }, { role: "zoom" }, { role: "close" }],
     },
   ];
 
